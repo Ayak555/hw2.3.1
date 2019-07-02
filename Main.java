7530241dfa3e34512feb45bd6aa4cb94221fba81
Ayak555 <51761545+Ayak555@users.noreply.github.com>
2019-07-02 15:19:36 +0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -11,7 +11,7 @@ public class Main {
         user.setName("Иванов Иван Иванович");
         user.setAge(50);
 
-        if (user.getAge() > 0 && user.getAge() < 100) {
+        while (user.getAge() > 0 && user.getAge() < 100) {
             try {
                 Scanner scanner = new Scanner(System.in);
                 int age = scanner.nextInt();
@@ -20,9 +20,7 @@ public class Main {
             } catch (Exception excep) {
                 System.out.println("ОШИБКА");
             }
-        } else {
         }
-
-
+  
     }
 }
